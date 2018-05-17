0aa7961963bca7f5eec97691985670061e493b30
user.email <abhijithnrajg@gmail.com>
2018-05-17 17:33:36 +0530

    program to perform lagrange's interpolation

--- lagrange_interpolation.py
+++ lagrange_interpolation.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+program_description='''program created to interpolate values from given set using lagrange's interpolation method'''
+author_name="Abhijith N Raj"
+org_name = "TKMCE"
+print(program_description)
+print("Created By "+author_name+ " of "+org_name)
 
+
+#reading values from stdin
 x=[float(i) for i in input("Enter x ").split()]
 y=[float(i) for i in input("Enter y ").split()]
 
@@ -11,14 +18,17 @@ def L(x_entered,i):
 			answer*=(x_entered-x[j])/(x[i]-x[j])
 	return answer
 
+#reading the value whose corresponding y value must be found
 x_entered = float(input("Enter the x value you want to find"))
 
+#lagranges's equation to find the polynomial
 def lagrange_interpolation_algorithm(x_entered):
 	solution=0
 	for i in range(len(x)):
 		solution+=L(x_entered,i)*y[i]
 	return solution
 
+#displaying the results of the calcualtions
 print("Solution is y("+str(x_entered)+")= "+ str(lagrange_interpolation_algorithm(x_entered)))
 
 
